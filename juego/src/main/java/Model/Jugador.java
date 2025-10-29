62ae40c2305a21c8e42af08da24a5feabaf51f73
Melany Gomez <melagoba91@gmail.com>
2025-10-29 00:19:06 -0500

    Update Jugador.java
    creación de atributos, constructor, getters y setters de la clase jugador

--- juego/src/main/java/Model/Jugador.java
+++ juego/src/main/java/Model/Jugador.java
@@ -1,4 +1,39 @@
 package Model;
-
+import java.util.ArrayList;
 public class Jugador {
+
+    protected String nombre;
+    protected ArrayList<Atacante> listaAtacantes;
+    protected Equipo equipo;
+
+    public Jugador(String nombre) {
+        this.nombre = nombre;
+        this.listaAtacantes = new ArrayList<>();
+        this.equipo = new Equipo();
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public ArrayList<Atacante> getListaAtacantes() {
+        return listaAtacantes;
+    }
+
+    public void setListaAtacantes(ArrayList<Atacante> listaAtacantes) {
+        this.listaAtacantes = listaAtacantes;
+    }
+
+    public Equipo getEquipo() {
+        return equipo;
+    }
+
+    public void setEquipo(Equipo equipo) {
+        this.equipo = equipo;
+    }
+    
 }
