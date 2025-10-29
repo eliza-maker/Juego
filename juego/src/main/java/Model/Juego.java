71aa22cc386cd017f9e3bbfe59f0f080d5f4260a
Melany Gomez <melagoba91@gmail.com>
2025-10-28 23:10:29 -0500

    clase Juego
    creación de atributos, constructor, getters y setters

--- juego/src/main/java/Model/Juego.java
+++ juego/src/main/java/Model/Juego.java
@@ -1,4 +1,39 @@
 package Model;
+import java.util.ArrayList;
 
 public class Juego {
+    protected String nombre;
+    protected ArrayList<Equipo> listaEquipos;
+    protected ArrayList<Jugador> listaJugadores;
+    public Juego(String nombre) {
+        this.nombre = nombre;
+        this.listaEquipos = new ArrayList<>();
+        this.listaJugadores = new ArrayList<>();
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public ArrayList<Equipo> getListaEquipos() {
+        return listaEquipos;
+    }
+
+    public void setListaEquipos(ArrayList<Equipo> listaEquipos) {
+        this.listaEquipos = listaEquipos;
+    }
+
+    public ArrayList<Jugador> getListaJugadores() {
+        return listaJugadores;
+    }
+
+    public void setListaJugadores(ArrayList<Jugador> listaJugadores) {
+        this.listaJugadores = listaJugadores;
+    }
+
+
 }
