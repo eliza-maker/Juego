847c3babddcb1fbdc0b6bc827240f7cd6efd5c8d
kevinRuiz69 <kevina.ruizc@uqvirtual.edu.co>
2025-10-30 14:15:01 -0500

    hola2
    ...

--- juego/src/main/java/Model/Kiriku.java
+++ juego/src/main/java/Model/Kiriku.java
@@ -1,7 +1,18 @@
 package Model;
 
+import java.util.List;
+
 public class Kiriku extends Atacante{
     public Kiriku(String nombre, int vida, int fuerza) {
         super(nombre, vida, fuerza);
+    }
+    @Override
+    public void atacar(List<Atacante> enemigos) {
+        for (Atacante enemigo : enemigos) {
+            if (enemigo.getVida() > 0) {
+                enemigo.recibirAtaque(getFuerza());
+                System.out.println(getNombre() + "Se esconde en las sombras y ataca a" + enemigo.getNombre() + " causando " + getFuerza() + " de daño.");
+            }
+        }
     }
 }
--- juego/src/main/java/Model/Mago.java
+++ juego/src/main/java/Model/Mago.java
@@ -1,8 +1,19 @@
 package Model;
 
+import java.util.List;
+
 public class Mago extends Atacante{
     public Mago(String nombre, int vida, int fuerza) {
         super(nombre, vida, fuerza);
+    }
+    @Override
+    public void atacar(List<Atacante> enemigos) {
+        for (Atacante enemigo : enemigos) {
+            if (enemigo.getVida() > 0) {
+                enemigo.recibirAtaque(getFuerza());
+                System.out.println(getNombre() + " lanza una bola de fuego a" + enemigo.getNombre() + " causando " + getFuerza() + " de daño.");
+            }
+        }
     }
 }
 
